1291749db66f32626b849ac265aacfc8a499824d
Colin Gross <grosscol@umich.edu>
2016-02-02 15:13:48 -0500

    Add config comment for web console whitelisting to dev env.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  
+  # Allow web console to subnet
+  # config.web_console.whitelisted_ips = '141.211.43.0/24'
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
