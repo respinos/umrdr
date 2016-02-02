21380d9550cc6bcd17297036d18e381ce0542aa8
Colin Gross <grosscol@umich.edu>
2016-02-02 10:50:21 -0500

    Add umrdr-dev and umrdr solr core configs.

---
+++ env_configs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# The environemnt variables that override the default config.
+export UMRDR_SOLR_DEV="http://localhost:8080/tomcat/quod-dev/solr-hydra/umrdr-dev" 
+export UMRDR_SOLR_TEST="http://localhost:8080/tomcat/quod-dev/solr-hydra/umrdr-dev"
+export UMRDR_BASE_DEV="/umrdr-dev"
+export UMRDR_BASE_TEST="/umrdr-dev"
